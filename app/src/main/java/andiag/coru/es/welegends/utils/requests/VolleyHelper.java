0e4b99f3020333b7f5ac06556f4c43b31dcbb65a
Andy <andyandy122444@gmail.com>
2015-06-26 00:07:06 +0200

    Añadida VOlley!

--- app/src/main/java/andiag/coru/es/welegends/utils/requests/VolleyHelper.java
+++ app/src/main/java/andiag/coru/es/welegends/utils/requests/VolleyHelper.java
@@ -1,9 +1,12 @@
 package andiag.coru.es.welegends.utils.requests;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 
 /**
@@ -12,11 +15,13 @@ import com.android.volley.toolbox.Volley;
 public class VolleyHelper {
     private static VolleyHelper INSTANCE;
     private RequestQueue requestQueue;
+    private ImageLoader imageLoader;
     private Context context;
 
     private VolleyHelper(Context context){
         this.context = context;
         requestQueue = getRequestQueue();
+        imageLoader = getImageLoader();
     }
 
     public static synchronized VolleyHelper getInstance(Context context){
@@ -31,6 +36,26 @@ public class VolleyHelper {
             requestQueue = Volley.newRequestQueue(context.getApplicationContext());
         }
         return requestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        if (imageLoader == null){
+            imageLoader = new ImageLoader(requestQueue, new ImageLoader.ImageCache() {
+                private final LruCache<String, Bitmap> cache = new LruCache<String, Bitmap>(20);
+
+
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return cache.get(url);
+                }
+
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                    cache.put(url, bitmap);
+                }
+            });
+        }
+        return imageLoader;
     }
 
     public <T> void addToRequestQueue(Request<T> req) {

c87e7680af9833014e9181632e1c323cc64934b4
Iago <iagocanalejas@gmail.com>
2015-06-26 12:49:13 +0200

    add DTO for summonerHistory and solved an error in historyhandler

--- app/src/main/java/andiag/coru/es/welegends/utils/history/HistoryHandler.java
+++ app/src/main/java/andiag/coru/es/welegends/utils/history/HistoryHandler.java
@@ -6,11 +6,12 @@ import android.content.SharedPreferences;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import andiag.coru.es.welegends.DTOs.SummonerHistory;
 import andiag.coru.es.welegends.entities.Summoner;
 
 /**
@@ -21,60 +22,78 @@ public abstract class HistoryHandler {
     private static final String HISTORY_FILE_NAME = "History";
     private static final int MAX_SUMMONERS_IN_HISTORY = 5;
 
-    public static Map<String, Summoner> getHistory(Activity activity) throws JSONException {
-        Map<String, Summoner> summoners = new HashMap<>();
+    public static ArrayList<SummonerHistory> getHistory(Activity activity) throws JSONException {
+        ArrayList<SummonerHistory> summoners = new ArrayList<>();
         Calendar limitTime = Calendar.getInstance();
-        limitTime.add(Calendar.DAY_OF_MONTH, -1);
+        limitTime.add(Calendar.DAY_OF_MONTH, -2);
 
         SharedPreferences settings = activity.getSharedPreferences(HISTORY_FILE_NAME, 0);
         Map<String, String> map = (Map<String, String>) settings.getAll();
 
+        if (map == null) {
+            return null;
+        }
+
         Set<String> keys = map.keySet();
 
         Summoner sum;
+        SummonerHistory summonerHistory;
+        JSONObject mainObject;
+        Calendar summonerTimeStamp;
+        long timestamp;
         for (String s : keys) {
             sum = new Summoner();
-            JSONObject mainObject = new JSONObject(s);
+            mainObject = new JSONObject(s);
 
-            long timestamp = mainObject.getLong("timestamp");
-            Calendar time = Calendar.getInstance();
-            time.setTimeInMillis(timestamp);
+            timestamp = mainObject.getLong("timestamp");
+            summonerTimeStamp = Calendar.getInstance();
+            summonerTimeStamp.setTimeInMillis(timestamp);
 
-            if (time.after(limitTime)) {
+            if (summonerTimeStamp.after(limitTime)) {
+                summonerHistory = new SummonerHistory();
                 sum.setId(mainObject.getLong("id"));
                 sum.setName(mainObject.getString("name"));
                 sum.setProfileIconId(mainObject.getLong("profileIconId"));
                 sum.setRevisionDate(mainObject.getLong("revisionDate"));
                 sum.setSummonerLevel(mainObject.getInt("summonerLevel"));
-                summoners.put(s, sum);
+
+                summonerHistory.setSummoner(sum);
+                summonerHistory.setTimestamp(summonerTimeStamp);
+
+                summoners.add(summonerHistory);
             }
         }
 
         return summoners;
     }
 
-    public static void setHistory(Activity activity, Map<String, Summoner> map) throws JSONException {
+    public static void setHistory(Activity activity, ArrayList<SummonerHistory> summoners) throws JSONException {
         SharedPreferences settings = activity.getSharedPreferences(HISTORY_FILE_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
+        Calendar limitTime = Calendar.getInstance();
+        limitTime.add(Calendar.DAY_OF_MONTH, -1);
 
-        Set<String> keys = map.keySet();
-
-        int count = 1;
+        int count = 0;
         JSONObject jo;
         Summoner sum;
-        for (String s : keys) {
+        Calendar summonerTimeStamp;
+        for (SummonerHistory summonerHistory : summoners) {
             if (count < MAX_SUMMONERS_IN_HISTORY) {
                 jo = new JSONObject();
-                sum = map.get(s);
+                sum = summonerHistory.getSummoner();
+                summonerTimeStamp = summonerHistory.getTimestamp();
 
                 jo.put("id", sum.getId());
                 jo.put("name", sum.getName());
                 jo.put("profileIconId", sum.getProfileIconId());
                 jo.put("revisionDate", sum.getRevisionDate());
                 jo.put("summonerLevel", sum.getSummonerLevel());
-                jo.put("timestamp", Calendar.getInstance().getTimeInMillis());
-
-                editor.putString(s, jo.toString());
+                if (summonerTimeStamp == null) {
+                    jo.put("timestamp", Calendar.getInstance().getTimeInMillis());
+                } else {
+                    jo.put("timestamp", summonerTimeStamp);
+                }
+                editor.putString(sum.getName(), jo.toString());
 
                 count++;
             } else {

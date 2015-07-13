3ddf0900d608509b217680b8cc43977cd94f3610
Iago <iagocanalejas@gmail.com>
2015-07-13 16:45:52 +0200

    animations put in animated activity

--- app/src/main/java/andiag/coru/es/welegends/activities/ActivityMain.java
+++ app/src/main/java/andiag/coru/es/welegends/activities/ActivityMain.java
@@ -1,120 +1,25 @@
 package andiag.coru.es.welegends.activities;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewCompat;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewConfiguration;
-import android.widget.FrameLayout;
-
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
-import com.github.ksoichiro.android.observablescrollview.ScrollUtils;
-import com.github.ksoichiro.android.observablescrollview.Scrollable;
-import com.github.ksoichiro.android.observablescrollview.TouchInterceptionFrameLayout;
-import com.nineoldandroids.animation.ValueAnimator;
-import com.nineoldandroids.view.ViewHelper;
-import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 import andiag.coru.es.welegends.R;
-import andiag.coru.es.welegends.activities.SuperActivities.TabbedActivity;
+import andiag.coru.es.welegends.activities.SuperActivities.AnimatedTabbedActivity;
 import andiag.coru.es.welegends.dialogs.DialogAbout;
 import andiag.coru.es.welegends.entities.Summoner;
 import andiag.coru.es.welegends.fragments.FragmentHistory;
 import andiag.coru.es.welegends.fragments.FragmentPlayerStats;
 import andiag.coru.es.welegends.utils.ViewServer;
 
-public class ActivityMain extends TabbedActivity implements ObservableScrollViewCallbacks {
-
-    private View mToolbarView;
-    private TouchInterceptionFrameLayout mInterceptionLayout;
-    private int mSlop;
-    private boolean mScrolled;
-    private ScrollState mLastScrollState;
-    private ActionBar actionBar;
+public class ActivityMain extends AnimatedTabbedActivity {
 
     private Summoner summoner;
     private String summonerName;
     private String region;
-    private TouchInterceptionFrameLayout.TouchInterceptionListener mInterceptionListener = new TouchInterceptionFrameLayout.TouchInterceptionListener() {
-        @Override
-        public boolean shouldInterceptTouchEvent(MotionEvent ev, boolean moving, float diffX, float diffY) {
-            if (!mScrolled && mSlop < Math.abs(diffX) && Math.abs(diffY) < Math.abs(diffX)) {
-                // Horizontal scroll is maybe handled by ViewPager
-                return false;
-            }
-
-            Scrollable scrollable = getCurrentScrollable();
-            if (scrollable == null) {
-                mScrolled = false;
-                return false;
-            }
-
-            // If interceptionLayout can move, it should intercept.
-            // And once it begins to move, horizontal scroll shouldn't work any longer.
-            int toolbarHeight = mToolbarView.getHeight();
-            int translationY = (int) ViewHelper.getTranslationY(mInterceptionLayout);
-            boolean scrollingUp = 0 < diffY;
-            boolean scrollingDown = diffY < 0;
-            if (scrollingUp) {
-                if (translationY < 0) {
-                    mScrolled = true;
-                    mLastScrollState = ScrollState.UP;
-                    return true;
-                }
-            } else if (scrollingDown) {
-                if (-toolbarHeight < translationY) {
-                    mScrolled = true;
-                    mLastScrollState = ScrollState.DOWN;
-                    return true;
-                }
-            }
-            mScrolled = false;
-            return false;
-        }
-
-        @Override
-        public void onDownMotionEvent(MotionEvent ev) {
-        }
-
-        @Override
-        public void onMoveMotionEvent(MotionEvent ev, float diffX, float diffY) {
-            float translationY = ScrollUtils.getFloat(ViewHelper.getTranslationY(mInterceptionLayout) + diffY, -mToolbarView.getHeight(), 0);
-            ViewHelper.setTranslationY(mInterceptionLayout, translationY);
-            if (translationY < 0) {
-                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mInterceptionLayout.getLayoutParams();
-                lp.height = (int) (-translationY + getScreenHeight());
-                mInterceptionLayout.requestLayout();
-            }
-        }
-
-        @Override
-        public void onUpOrCancelMotionEvent(MotionEvent ev) {
-            mScrolled = false;
-            adjustToolbar(mLastScrollState);
-        }
-    };
-
-    static int blendColors(int from, int to, float ratio) {
-        final float inverseRation = 1f - ratio;
-        final float r = Color.red(from) * ratio + Color.red(to) * inverseRation;
-        final float g = Color.green(from) * ratio + Color.green(to) * inverseRation;
-        final float b = Color.blue(from) * ratio + Color.blue(to) * inverseRation;
-        return Color.rgb((int) r, (int) g, (int) b);
-    }
 
     public String getRegion() {
         return region;
@@ -183,13 +88,8 @@ public class ActivityMain extends TabbedActivity implements ObservableScrollView
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_main);
 
+        //NEED THIS TO USE SETANIMATION METHOD
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-
-        final ColorDrawable actionBarBackground = new ColorDrawable();
-        final ColorDrawable actionBarTabsColor = new ColorDrawable();
-        actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(actionBarBackground);
-        actionBar.setDisplayHomeAsUpEnabled(true);
 
         //GETTING DATA AND SET TABS TO VIEW PAGER
         onRetrieveInstanceState(savedInstanceState);
@@ -199,52 +99,8 @@ public class ActivityMain extends TabbedActivity implements ObservableScrollView
         FragmentHistory.getInstance(this).setSummoner_id(summoner.getId(), region);
         FragmentPlayerStats.getInstance(this).setSummoner(summoner);
 
-        ViewCompat.setElevation(findViewById(R.id.header), getResources().getDimension(R.dimen.toolbar_elevation));
-        mToolbarView = findViewById(R.id.toolbar);
+        setAnimation();
 
-        // Padding for ViewPager must be set outside the ViewPager itself
-        // because with padding, EdgeEffect of ViewPager become strange.
-        final int tabHeight = getResources().getDimensionPixelSize(R.dimen.tab_height);
-        findViewById(R.id.pager_wrapper).setPadding(0, getActionBarSize() + tabHeight, 0, 0);
-
-        SmartTabLayout slidingTabLayout = (SmartTabLayout) findViewById(R.id.sliding_tabs);
-        slidingTabLayout.setBackground(actionBarTabsColor);
-        slidingTabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
-        slidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.white_20));
-        slidingTabLayout.setViewPager(mPager);
-        slidingTabLayout.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                //actionBar.setSelectedNavigationItem(position);
-            }
-
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                if (position >= mPagerAdapter.getCount() - 1) {
-                    return;
-                }
-                int from = mPagerAdapter.getColorActionBar(position);
-                int to = mPagerAdapter.getColorActionBar(position + 1);
-
-                int fromT = mPagerAdapter.getColorToolBar(position);
-                int toT = mPagerAdapter.getColorToolBar(position + 1);
-
-                final int blended = blendColors(to, from, positionOffset);
-                final int blendedT = blendColors(toT, fromT, positionOffset);
-                actionBarBackground.setColor(blended);
-                actionBarTabsColor.setColor(blended);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setStatusBarColor(blendedT);
-                }
-            }
-        });
-
-        ViewConfiguration vc = ViewConfiguration.get(this);
-        mSlop = vc.getScaledTouchSlop();
-        mInterceptionLayout = (TouchInterceptionFrameLayout) findViewById(R.id.container);
-        mInterceptionLayout.setScrollInterceptionListener(mInterceptionListener);
-
-        ViewServer.get(this).addWindow(this);
     }
 
     @Override
@@ -278,12 +134,8 @@ public class ActivityMain extends TabbedActivity implements ObservableScrollView
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_about) {
             DialogAbout dialogAbout = DialogAbout.newInstance();
             dialogAbout.show(getSupportFragmentManager(), "DialogAbout");
@@ -294,110 +146,5 @@ public class ActivityMain extends TabbedActivity implements ObservableScrollView
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-    }
-
-    @Override
-    public void onDownMotionEvent() {
-    }
-
-    @Override
-    public void onUpOrCancelMotionEvent(ScrollState scrollState) {
-        if (!mScrolled) {
-            // This event can be used only when TouchInterceptionFrameLayout
-            // doesn't handle the consecutive events.
-            adjustToolbar(scrollState);
-        }
-    }
-
-    protected int getActionBarSize() {
-        TypedValue typedValue = new TypedValue();
-        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
-        int indexOfAttrTextSize = 0;
-        TypedArray a = obtainStyledAttributes(typedValue.data, textSizeAttr);
-        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
-        a.recycle();
-        return actionBarSize;
-    }
-
-    protected int getScreenHeight() {
-        return findViewById(android.R.id.content).getHeight();
-    }
-
-    private Scrollable getCurrentScrollable() {
-        Fragment fragment = getCurrentFragment();
-        if (fragment == null) {
-            return null;
-        }
-        View view = fragment.getView();
-        if (view == null) {
-            return null;
-        }
-        return (Scrollable) view.findViewById(R.id.scroll);
-    }
-
-    private void adjustToolbar(ScrollState scrollState) {
-        int toolbarHeight = mToolbarView.getHeight();
-        final Scrollable scrollable = getCurrentScrollable();
-        if (scrollable == null) {
-            return;
-        }
-        int scrollY = scrollable.getCurrentScrollY();
-        if (scrollState == ScrollState.DOWN) {
-            showToolbar();
-        } else if (scrollState == ScrollState.UP) {
-            if (toolbarHeight <= scrollY) {
-                hideToolbar();
-            } else {
-                showToolbar();
-            }
-        } else if (!toolbarIsShown() && !toolbarIsHidden()) {
-            // Toolbar is moving but doesn't know which to move:
-            // you can change this to hideToolbar()
-            showToolbar();
-        }
-    }
-
-    private Fragment getCurrentFragment() {
-        return mPagerAdapter.getItemAt(mPager.getCurrentItem());
-    }
-
-    private boolean toolbarIsShown() {
-        return ViewHelper.getTranslationY(mInterceptionLayout) == 0;
-    }
-
-    private boolean toolbarIsHidden() {
-        return ViewHelper.getTranslationY(mInterceptionLayout) == -mToolbarView.getHeight();
-    }
-
-    private void showToolbar() {
-        animateToolbar(0);
-    }
-
-    private void hideToolbar() {
-        animateToolbar(-mToolbarView.getHeight());
-    }
-
-    private void animateToolbar(final float toY) {
-        float layoutTranslationY = ViewHelper.getTranslationY(mInterceptionLayout);
-        if (layoutTranslationY != toY) {
-            ValueAnimator animator = ValueAnimator.ofFloat(ViewHelper.getTranslationY(mInterceptionLayout), toY).setDuration(200);
-            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    float translationY = (float) animation.getAnimatedValue();
-                    ViewHelper.setTranslationY(mInterceptionLayout, translationY);
-                    if (translationY < 0) {
-                        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mInterceptionLayout.getLayoutParams();
-                        lp.height = (int) (-translationY + getScreenHeight());
-                        mInterceptionLayout.requestLayout();
-                    }
-                }
-            });
-            animator.start();
-        }
     }
 }

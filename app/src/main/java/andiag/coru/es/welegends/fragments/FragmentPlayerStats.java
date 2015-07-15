4f7288d57a6f18b9494a7e4e352626e674471120
Iago <iagocanalejas@gmail.com>
2015-07-15 17:16:33 +0200

    now we have the stats in our fragment

--- app/src/main/java/andiag/coru/es/welegends/fragments/FragmentPlayerStats.java
+++ app/src/main/java/andiag/coru/es/welegends/fragments/FragmentPlayerStats.java
@@ -2,12 +2,24 @@ package andiag.coru.es.welegends.fragments;
 
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.gson.Gson;
 
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+
+import andiag.coru.es.welegends.DTOs.rankedStatsDTOs.RankedStatsDto;
 import andiag.coru.es.welegends.R;
 import andiag.coru.es.welegends.activities.ActivityMain;
 import andiag.coru.es.welegends.entities.Summoner;
@@ -28,6 +40,7 @@ public class FragmentPlayerStats extends SwipeRefreshLayoutFragment {
     private ImageLoader imageLoader;
     private APIHandler apiHandler;
     private Summoner summoner;
+    private boolean isLoading;
 
     public FragmentPlayerStats() {
         imageLoader = VolleyHelper.getInstance(getActivity()).getImageLoader();
@@ -49,13 +62,6 @@ public class FragmentPlayerStats extends SwipeRefreshLayoutFragment {
 
     public void setSummoner(Summoner summoner) {
         this.summoner = summoner;
-
-        if (rootView != null) {
-            CircledNetworkImageView networkImg = (CircledNetworkImageView) rootView.findViewById(R.id.imageSummoner);
-            networkImg.setErrorImageResId(R.drawable.item_default);
-            networkImg.setDefaultImageResId(R.drawable.item_default);
-            networkImg.setImageUrl(apiHandler.getServer() + apiHandler.getIcon() + summoner.getProfileIconId(), imageLoader);
-        }
     }
 
     @Override
@@ -71,12 +77,64 @@ public class FragmentPlayerStats extends SwipeRefreshLayoutFragment {
         rootView = inflater.inflate(R.layout.fragment_playerstats, container, false);
 
         if (summoner != null) {
+            Log.d("INITIALIZE", "ON CREATE VIEW");
             CircledNetworkImageView networkImg = (CircledNetworkImageView) rootView.findViewById(R.id.imageSummoner);
             networkImg.setErrorImageResId(R.drawable.item_default);
             networkImg.setDefaultImageResId(R.drawable.item_default);
             networkImg.setImageUrl(apiHandler.getServer() + apiHandler.getIcon() + summoner.getProfileIconId(), imageLoader);
+
+            getStats();
         }
 
         return rootView;
     }
+
+    private void getStats() {
+        if (isLoading) return;
+
+        isLoading = true;
+
+        final Gson gson = new Gson();
+
+        changeRefreshingValue(true);
+        APIHandler handler = APIHandler.getInstance();
+        if (handler == null) {
+            handler = APIHandler.getInstance(activityMain);
+        }
+
+        String request = handler.getServer() + handler.getStats() + summoner.getId() + handler.getStats_ranked();
+        //String request = handler.getServer() + handler.getStats() + summoner.getId() + handler.getStats_global();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, request, (String) null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        RankedStatsDto rankedStatsDto = gson.fromJson(String.valueOf(response), RankedStatsDto.class);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR", error.toString());
+                isLoading = false;
+                changeRefreshingValue(false);
+                NetworkResponse networkResponse = error.networkResponse;
+                if (networkResponse != null) {
+                    String message = activityMain.getString(R.string.errorDefault);
+                    switch (networkResponse.statusCode) {
+                        case HttpStatus.SC_INTERNAL_SERVER_ERROR:
+                            message = activityMain.getString(R.string.error500);
+                            break;
+                        case HttpStatus.SC_SERVICE_UNAVAILABLE:
+                            message = activityMain.getString(R.string.error503);
+                            break;
+                    }
+                    Toast.makeText(activityMain, message
+                            , Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        VolleyHelper.getInstance(activityMain).getRequestQueue().add(jsonObjectRequest);
+    }
+
 }

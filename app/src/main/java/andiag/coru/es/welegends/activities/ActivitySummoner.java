9793e520c216025bf2d895393ae887015b4d9243
Iago Santos Domínguez <iagocanalejas@gmail.com>
2016-06-14 19:41:16 +0200

    Split activity summoner in 2 fragments

--- app/src/main/java/andiag/coru/es/welegends/activities/ActivitySummoner.java
+++ app/src/main/java/andiag/coru/es/welegends/activities/ActivitySummoner.java
@@ -1,147 +1,61 @@
 package andiag.coru.es.welegends.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AdapterView;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import andiag.coru.es.welegends.R;
-import andiag.coru.es.welegends.Utils;
+import andiag.coru.es.welegends.fragments.FragmentFindSummoner;
 import andiag.coru.es.welegends.persistence.DBSummoner;
-import andiag.coru.es.welegends.rest.RestClient;
 import andiag.coru.es.welegends.rest.entities.Summoner;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-public class ActivitySummoner extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class ActivitySummoner extends AppCompatActivity {
 
     private final static String TAG = "ActivitySummoner";
+    private final static String FIND_SUMMONER_FRAGMENT = "findSummonerFragment";
+    private final static String SUMMONER_HISTORIC_FRAGMENT = "summonerHistoricFragment";
 
-    private String region;
     private DBSummoner db;
 
-    //region Callbacks
-
-    private Callback<Summoner> callbackSearchSummoner = new Callback<Summoner>() {
-        @Override
-        public void onResponse(Call<Summoner> call, Response<Summoner> response) {
-            Log.d(TAG, "onClickFindSummoner: Found-" + response.body().getId());
-            Summoner summoner = response.body();
-            summoner.setRegion(region);
-            Log.d(TAG, "onClickFindSummoner: Updating database");
-            db.addSummoner(summoner);
-            //TODO load new screen #1
-        }
-        @Override
-        public void onFailure(Call<Summoner> call, Throwable t) {
-            Log.d(TAG, "onClickFindSummoner: Summoner not found");
-            Toast.makeText(getApplicationContext(), getString(R.string.error404),
-                    Toast.LENGTH_LONG).show();
-        }
-    };
-    private Callback<Summoner> callbackUpdateSummoner = new Callback<Summoner>() {
-        @Override
-        public void onResponse(Call<Summoner> call, Response<Summoner> response) {
-            //TODO move this to background processing
-            Log.d(TAG, "onClickFindSummoner: Found-" + response.body().getId());
-            Summoner newSummoner = response.body();
-            newSummoner.setRegion(region);
-            //TODO notify fragment
-        }
-        @Override
-        public void onFailure(Call<Summoner> call, Throwable t) {
-            //This case may never happen
-            Log.d(TAG, "onClickFindSummoner: Summoner not found");
-        }
-    };
-
-    //endregion
+    public DBSummoner getDb() {
+        return db;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_summoner_new);
+        setContentView(R.layout.activity_summoner_fragments);
         db = DBSummoner.getInstance(this);
 
-        //Allow press Enter key to search
-        startSummonerListener();
-
-        //Region picker
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerRegions);
-        spinner.setOnItemSelectedListener(this);
-
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        region = (String) parent.getItemAtPosition(position);
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-    }
-
-    private void startSummonerListener(){
-        EditText editText = (EditText) findViewById(R.id.editTextSummoner);
-        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if ((actionId == EditorInfo.IME_ACTION_DONE) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN))) {
-                    onClickFindSummoner(v);
-                    return true;
-                }
-                return false;
-            }
-        });
-    }
-
-    private void apiSearchSummonerId(String summonerName, String region, Callback<Summoner> callback){
-        Call<Summoner> call = RestClient.get(summonerName).getSummonerByName(region, summonerName);
-        call.enqueue(callback);
-    }
-
-    public void onClickFindSummoner(View v){
-        //Get and clean summoner
-        String summonerName = ((EditText) findViewById(R.id.editTextSummoner)).getText().toString();
-        summonerName = Utils.cleanString(summonerName);
-
-        if(summonerName.isEmpty() || summonerName.equals("")){
-            Log.e(TAG, "onClickFindSummoner: Wrong search text");
-            Toast.makeText(getApplicationContext(), getString(R.string.voidSummonerError),
-                    Toast.LENGTH_LONG).show();
-            return;
+        if (findViewById(R.id.fragmentContainer) != null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new FragmentFindSummoner(), FIND_SUMMONER_FRAGMENT).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragmentFindSummoner, new FragmentFindSummoner(), FIND_SUMMONER_FRAGMENT)
+                    .add(R.id.fragmentSummonerHistoric, new Fragment(), SUMMONER_HISTORIC_FRAGMENT)
+                    .commit();
         }
 
-        //Check if network is available
-        if (!Utils.isNetworkAvailable(this)){
-            Log.e(TAG, "onClickFindSummoner: Network unavailable");
-            Toast.makeText(this, getString(R.string.networkUnavailable), Toast.LENGTH_LONG).show();
-            return;
-        }
+    }
 
-        //Try to find summoner in local database
-        Log.d(TAG,"onClickFindSummoner: Searching " + summonerName + " " + region);
-        final Summoner summoner = db.getSummonerByName(summonerName, region);
-
-        if(summoner==null){ //We don't -> ask to the API
-            apiSearchSummonerId(summonerName, region, callbackSearchSummoner);
-            return;
-        }
-
-        //We do -> end.
-        Log.d(TAG, "onClickFindSummoner: FoudInLocal-" + summoner.getId());
-        Log.d(TAG, "onClickFindSummoner: Updating database");
-        db.addSummoner(summoner);
+    public void throwNewActivity(Summoner summoner) {
         //TODO load new screen #1
-        apiSearchSummonerId(summonerName, region, callbackUpdateSummoner);
     }
 
+    //Redirect View action to fragment
+    public void onClickFindSummoner(View v){
+        FragmentFindSummoner fragmentFindSummoner = (FragmentFindSummoner) getSupportFragmentManager()
+                .findFragmentByTag(FIND_SUMMONER_FRAGMENT);
+
+        fragmentFindSummoner.findSummoner(fragmentFindSummoner.getView());
+    }
+
+    public void onClickSwapFragment(View view) {
+        if (findViewById(R.id.fragmentContainer) != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new Fragment())
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
 }
---
+++ app/src/main/java/andiag/coru/es/welegends/fragments/FragmentFindSummoner.java
@@ -0,0 +1,168 @@
+package andiag.coru.es.welegends.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import andiag.coru.es.welegends.R;
+import andiag.coru.es.welegends.Utils;
+import andiag.coru.es.welegends.activities.ActivitySummoner;
+import andiag.coru.es.welegends.persistence.DBSummoner;
+import andiag.coru.es.welegends.rest.RestClient;
+import andiag.coru.es.welegends.rest.entities.Summoner;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class FragmentFindSummoner extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    private final static String TAG = "FragmentFindSummoner";
+
+    private ActivitySummoner parentActivity;
+    private DBSummoner db;
+
+    private String region;
+
+    //region Callbacks
+    private Callback<Summoner> callbackSearchSummoner = new Callback<Summoner>() {
+        @Override
+        public void onResponse(Call<Summoner> call, Response<Summoner> response) {
+            Log.d(TAG, "onClickFindSummoner: Found-" + response.body().getId());
+            Summoner summoner = response.body();
+            summoner.setRegion(region);
+            Log.d(TAG, "onClickFindSummoner: Updating database");
+            db.addSummoner(summoner);
+            parentActivity.throwNewActivity(summoner);
+        }
+
+        @Override
+        public void onFailure(Call<Summoner> call, Throwable t) {
+            Log.d(TAG, "onClickFindSummoner: Summoner not found");
+            Toast.makeText(parentActivity.getApplicationContext(), getString(R.string.error404),
+                    Toast.LENGTH_LONG).show();
+        }
+    };
+    private Callback<Summoner> callbackUpdateSummoner = new Callback<Summoner>() {
+        @Override
+        public void onResponse(Call<Summoner> call, Response<Summoner> response) {
+            //TODO move this to background processing
+            Log.d(TAG, "onClickFindSummoner: Found-" + response.body().getId());
+            Summoner newSummoner = response.body();
+            newSummoner.setRegion(region);
+            //TODO notify fragment
+        }
+
+        @Override
+        public void onFailure(Call<Summoner> call, Throwable t) {
+            //This case may never happen
+            Log.d(TAG, "onClickFindSummoner: Summoner not found");
+        }
+    };
+    //endregion
+
+    public FragmentFindSummoner() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.parentActivity = (ActivitySummoner) context;
+        this.db = ((ActivitySummoner) context).getDb();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_find_summoner, container, false);
+
+        //Allow press Enter key to search
+        startSummonerListener(fragmentView);
+
+        //Region picker
+        Spinner spinner = (Spinner) fragmentView.findViewById(R.id.spinnerRegions);
+        spinner.setOnItemSelectedListener(this);
+
+        return fragmentView;
+    }
+
+    //region OnItemSelectedListener implementation
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        region = (String) parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
+    //endregion
+
+    private void startSummonerListener(final View fragmentView) {
+        EditText editText = (EditText) fragmentView.findViewById(R.id.editTextSummoner);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((actionId == EditorInfo.IME_ACTION_DONE) || ((event.getKeyCode() == KeyEvent.KEYCODE_ENTER) && (event.getAction() == KeyEvent.ACTION_DOWN))) {
+                    findSummoner(fragmentView);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void apiSearchSummonerId(String summonerName, String region, Callback<Summoner> callback) {
+        Call<Summoner> call = RestClient.get(summonerName).getSummonerByName(region, summonerName);
+        call.enqueue(callback);
+    }
+
+    public void findSummoner(View fragmentView) {
+        //Get and clean summoner
+        String summonerName = ((EditText) fragmentView.findViewById(R.id.editTextSummoner)).getText().toString();
+        summonerName = Utils.cleanString(summonerName);
+
+        if (summonerName.isEmpty() || summonerName.equals("")) {
+            Log.e(TAG, "onClickFindSummoner: Wrong search text");
+            Toast.makeText(parentActivity.getApplicationContext(), getString(R.string.voidSummonerError),
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //Check if network is available
+        if (!Utils.isNetworkAvailable(parentActivity)) {
+            Log.e(TAG, "onClickFindSummoner: Network unavailable");
+            Toast.makeText(parentActivity.getApplicationContext(), getString(R.string.networkUnavailable),
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        //Try to find summoner in local database
+        Log.d(TAG, "onClickFindSummoner: Searching " + summonerName + " " + region);
+        final Summoner summoner = db.getSummonerByName(summonerName, region);
+
+        if (summoner == null) { //We don't -> ask to the API
+            apiSearchSummonerId(summonerName, region, callbackSearchSummoner);
+            return;
+        }
+
+        //We do -> end.
+        Log.d(TAG, "onClickFindSummoner: FoudInLocal-" + summoner.getId());
+        Log.d(TAG, "onClickFindSummoner: Updating database");
+        db.addSummoner(summoner);
+        apiSearchSummonerId(summonerName, region, callbackUpdateSummoner);
+        parentActivity.throwNewActivity(summoner);
+    }
+
+}

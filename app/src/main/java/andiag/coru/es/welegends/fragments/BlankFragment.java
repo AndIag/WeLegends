8525766777acf2aacee521d61105e4f3b71d3c09
Andy Quintero Melo <andyandy122444@gmail.com>
2016-06-28 19:10:43 +0200

    Creating skeleton of the Main Activity.

---
+++ app/src/main/java/andiag/coru/es/welegends/fragments/BlankFragment.java
@@ -0,0 +1,31 @@
+package andiag.coru.es.welegends.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import andiag.coru.es.welegends.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}

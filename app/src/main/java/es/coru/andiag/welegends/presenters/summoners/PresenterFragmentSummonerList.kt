65ce5e16008ba68b51f4e0fba41336cc85794015
Andy Quintero Melo <andyandy122444@gmail.com>
2016-12-15 22:21:10 +0100

    Creating view, presenter and model call to setup FragmentSummonerList

---
+++ app/src/main/java/es/coru/andiag/welegends/presenters/summoners/PresenterFragmentSummonerList.kt
@@ -0,0 +1,47 @@
+package es.coru.andiag.welegends.presenters.summoners
+
+import android.content.Context
+import es.coru.andiag.andiag_mvp.presenters.AIFragmentPresenter
+import es.coru.andiag.andiag_mvp.presenters.AIInterfaceLoaderPresenter
+import es.coru.andiag.welegends.R
+import es.coru.andiag.welegends.models.wrapped.database.Summoner
+import es.coru.andiag.welegends.views.summoners.impl.ActivitySummoners
+import es.coru.andiag.welegends.views.summoners.impl.FragmentSummonerList
+
+/**
+ * Created by andyq on 15/12/2016.
+ */
+class PresenterFragmentSummonerList : AIFragmentPresenter<FragmentSummonerList, ActivitySummoners>(), AIInterfaceLoaderPresenter<List<Summoner>> {
+
+    override fun onViewAttached() {
+        loadSummoners()
+    }
+
+    private fun loadSummoners() {
+
+    }
+
+    override fun onViewDetached() {
+    }
+
+    //region AIInterfaceLoaderPresenter
+    override fun getContext(): Context {
+        return parent!!
+    }
+
+    override fun onLoadSuccess(message: String?, data: List<Summoner>) {
+        view!!.onSummonersLoaded(data)
+    }
+
+    override fun onLoadProgressChange(message: String?, data: List<Summoner>) {
+    }
+
+    override fun onLoadError(message: String?) {
+        view!!.onSummonersEmpty(R.string.error404)
+    }
+
+    override fun onLoadError(resId: Int) {
+        view!!.onSummonersEmpty(resId)
+    }
+    //endregion
+}
--- app/src/main/java/es/coru/andiag/welegends/views/summoners/impl/FragmentSummonerList.kt
+++ app/src/main/java/es/coru/andiag/welegends/views/summoners/impl/FragmentSummonerList.kt
@@ -1,7 +1,58 @@
 package es.coru.andiag.welegends.views.summoners.impl
 
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.Toast
+import butterknife.BindView
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.listener.OnItemClickListener
+import es.coru.andiag.welegends.R
+import es.coru.andiag.welegends.common.base.FragmentBase
+import es.coru.andiag.welegends.models.wrapped.database.Summoner
+import es.coru.andiag.welegends.presenters.summoners.PresenterFragmentSummonerList
+import es.coru.andiag.welegends.views.adapters.AdapterSummonerList
+import es.coru.andiag.welegends.views.summoners.ViewFragmentSummonerList
+import java.util.*
+
+
 /**
  * Created by andyq on 09/12/2016.
  */
-class FragmentSummonerList {
+class FragmentSummonerList : FragmentBase<PresenterFragmentSummonerList>(), ViewFragmentSummonerList {
+
+    @BindView(R.id.recyclerSummoners)
+    var recycler: RecyclerView? = null
+
+    var adapter: AdapterSummonerList? = null
+
+    override val fragmentLayout: Int = R.layout.fragment_summoner_list
+
+    override fun setupViews() {
+        recycler!!.setHasFixedSize(true)
+        recycler!!.layoutManager = LinearLayoutManager(context)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        adapter = AdapterSummonerList(R.layout.item_summoner_historic, ArrayList<Summoner>())
+        adapter!!.openLoadAnimation()
+        recycler!!.adapter = adapter
+        recycler!!.addOnItemTouchListener(object : OnItemClickListener() {
+            override fun onSimpleItemClick(p0: BaseQuickAdapter<*, *>?, p1: View?, p2: Int) {
+                val summoner : Summoner = p0!!.getItem(p2) as Summoner
+            }
+
+        })
+
+    }
+
+    override fun onSummonersLoaded(summoners: List<Summoner>) {
+        adapter!!.setNewData(summoners)
+    }
+
+    override fun onSummonersEmpty(error: Int?) {
+        adapter!!.setNewData(null)
+        Toast.makeText(context, error!!, Toast.LENGTH_SHORT).show()
+    }
 }
